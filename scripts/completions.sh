a106c3d6a4c6eb9dccc8309d33192b0a89940536
xavidop <xavi_tb@hotmail.com>
2024-12-31 15:29:14 +0100

    fix: release

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
+set -x
+set -v
 rm -rf completions
+
 mkdir completions
 for sh in bash zsh fish; do
 	go run main.go completion "$sh" >"completions/voiceflow.$sh"
