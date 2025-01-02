9322ae53d34afe706bae3a1122d0c0c099b86f83
xavidop <xavi_tb@hotmail.com>
2025-01-02 20:00:17 +0100

    fix: jsonschemas

--- internal/types/tests/suiteTypes.go
+++ internal/types/tests/suiteTypes.go
@@ -1,13 +1,13 @@
 package tests
 
 type Suite struct {
-	Name            string     `yaml:"name"`
-	Description     string     `yaml:"description"`
-	EnvironmentName string     `yaml:"environmentName"`
-	Tests           []TestFile `yaml:"tests"`
+	Name            string     `yaml:"name" json:"name"`
+	Description     string     `yaml:"description" json:"description"`
+	EnvironmentName string     `yaml:"environmentName" json:"environmentName"`
+	Tests           []TestFile `yaml:"tests" json:"tests"`
 }
 
 type TestFile struct {
-	ID   string `yaml:"id"`
-	File string `yaml:"file"`
+	ID   string `yaml:"id" json:"id"`
+	File string `yaml:"file" json:"file"`
 }
--- internal/types/tests/testTypes.go
+++ internal/types/tests/testTypes.go
@@ -1,28 +1,28 @@
 package tests
 
 type Test struct {
-	Name         string        `yaml:"name"`
-	Description  string        `yaml:"description"`
-	Interactions []Interaction `yaml:"interactions"`
+	Name         string        `yaml:"name" json:"name"`
+	Description  string        `yaml:"description" json:"description"`
+	Interactions []Interaction `yaml:"interactions" json:"interactions"`
 }
 
 type Interaction struct {
-	ID    string `yaml:"id"`
-	User  User   `yaml:"user"`
-	Agent Agent  `yaml:"agent"`
+	ID    string `yaml:"id" json:"id"`
+	User  User   `yaml:"user" json:"user"`
+	Agent Agent  `yaml:"agent" json:"agent"`
 }
 
 type User struct {
-	Type string `yaml:"type"`
-	Text string `yaml:"text,omitempty"`
+	Type string `yaml:"type" json:"type"`
+	Text string `yaml:"text,omitempty" json:"text,omitempty"`
 }
 
 type Agent struct {
-	Validate []Validation `yaml:"validate"`
+	Validate []Validation `yaml:"validate" json:"validate"`
 }
 
 type Validation struct {
-	ID    string `yaml:"id"`
-	Type  string `yaml:"type"`
-	Value string `yaml:"value"`
+	ID    string `yaml:"id" json:"id"`
+	Type  string `yaml:"type" json:"type"`
+	Value string `yaml:"value" json:"value"`
 }
